bcbb9573b3e439440939e46347d85be78279ecc9
msn13 <mason.murphy.buz@gmail.com>
2026-01-08 12:57:04 -0600

    Project Init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import config from "./postcss.config.mjs";
+
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans:['Roboto', 'sans-serif'],
+            }
+        },
+    },
+    plugins: [],
+    config
+};
